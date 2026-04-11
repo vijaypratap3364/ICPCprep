645c0ebd5cbf2cd6c642c864a47875559b330a50
vijaypratap3364 <singhvijaypratap1011@gmail.com>
2026-04-10 20:43:46 -0500

    C++ and Basic Maths completed

--- demo.cpp
+++ demo.cpp
@@ -1,8 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void pattern(int n) {
+    for (int i=1; i<=n; i++){
+        for (int j=1; j<=i; j++){
+            cout<< j;
+        } for (int j=1; j<=2*(n-i); j++){
+            cout<< " ";
+        } for (int j = 1; j<=i; j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+}
+
 int main() {
-    
+    int n;
+    cin >> n;
+    pattern(n);
 
     return 0;
 }
